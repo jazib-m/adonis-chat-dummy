cb9dd9227646f582d062123567060d1fecab6173
jazib <jazib.mehmood@clickchain.com>
2019-06-16 03:13:45 +0500

    added events

--- chat-app-api/start/socket.js
+++ chat-app-api/start/socket.js
@@ -3,4 +3,9 @@ const io = use('socket.io')(Server.getInstance())
 
 io.on('connection', function (socket) {
   console.log(socket.id);
+
+  socket.on("join", (room) =>{ socket.join(room)});
+
+  socket.on("message", (room,message) =>{ socket.on(room).broadcast(message)});
+
 })
